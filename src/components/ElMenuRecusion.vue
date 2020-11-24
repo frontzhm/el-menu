f9c97c42d260136c0117d67186ed7d467807a3f6
zhm <frontzhm@163.com>
2020-11-24 15:00:16 +0800

    feat: add recusion component

---
+++ src/components/ElMenuRecusion.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  el-submenu(v-if="item.children && item.children.length")
+    template(#title) {{item.title}}
+    el-menu-recusion(v-for="(innerItem,i) in item.children" :item="innerItem" :key="i")
+  el-menu-item(v-else) {{item.title}}
+</template>
+<script>
+import ElMenuItem from "./ElMenuItem.vue";
+import ElSubmenu from "./ElSubmenu";
+export default {
+  name: "ElMenuRecusion",
+  components: {
+    ElMenuItem,
+    ElSubmenu
+  },
+  props: {
+    item: Object
+  }
+};
+</script>
--- src/components/MyMenu.vue
+++ src/components/MyMenu.vue
@@ -1,22 +1,19 @@
 <template lang="pug">
   el-menu
-    el-submenu
-      template(#title) 折叠菜单标题(有箭头)
-      el-menu-item 子菜单1
-      el-menu-item 子菜单2
-    el-menu-item 菜单1
-    el-menu-item 菜单2  
+    el-menu-recusion(v-for="(item,index) in data" :item="item"  :key="index")
 </template>
 <script>
 import ElMenu from "./ElMenu.vue";
 import ElMenuItem from "./ElMenuItem.vue";
 import ElSubmenu from "./ElSubmenu";
+import ElMenuRecusion from "./ElMenuRecusion";
 export default {
   name: "MyMenu",
   components: {
     ElMenu,
     ElMenuItem,
-    ElSubmenu
+    ElSubmenu,
+    ElMenuRecusion
   },
   props: {
     data: Array
